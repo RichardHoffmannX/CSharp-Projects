65e00c0ef09c499907732451e9a0d08791132408
Richard Hoffmann <95046844+RichardHoffmannX@users.noreply.github.com>
2025-12-26 06:29:10 +0100

    Added first tutorial Hello World

---
+++ 001_HelloWorld/Tutorial1.cs
@@ -0,0 +1,18 @@
+// Learn C# (C-Sharp)
+// C# is a programming language developed by Microsoft that runs on the .NET Framework.
+
+// C# is used to develop web apps, desktop apps, mobile apps, games and so much more.
+// Tutorial 1. Hello World: Famous first Hello World Example!
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");    
+    }
+  }
+}
